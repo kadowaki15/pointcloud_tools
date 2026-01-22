e9a6f9165702859c256f0bfdd844d0c9e3e0e786
Reina Kadowaki <reopi0812@gmail.com>
2026-01-22 21:32:55 +0900

    0122

---
+++ launch/intent_tracking.launch.py
@@ -0,0 +1,49 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='pointcloud_tools',
+            executable='ego_compensator',
+            name='ego_compensator',
+            output='screen',
+        ),
+        Node(
+            package='pointcloud_tools',
+            executable='dynamic_cluster_node',
+            name='dynamic_cluster_node',
+            output='screen',
+        ),
+        Node(
+            package='pointcloud_tools',
+            executable='tracker',
+            name='tracker',
+            output='screen',
+        ),
+        Node(
+            package='pointcloud_tools',
+            executable='intent_estimator',
+            name='intent_estimator',
+            output='screen',
+        ),
+        Node(
+            package='pointcloud_tools',
+            executable='safety_interface',
+            name='safety_interface',
+            output='screen',
+        ),
+        Node(
+            package='pointcloud_tools',
+            executable='static_obstacle_detector',
+            name='static_obstacle_detector',
+            output='screen',
+        ),
+        Node(
+            package='pointcloud_tools',
+            executable='rgb_object_locator',
+            name='rgb_object_locator',
+            output='screen',
+        ),
+       
+    ])
---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import find_packages, setup
+
+package_name = 'pointcloud_tools'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='kadowaki',
+    maintainer_email='reopi0812@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        'pointcloud_diff = pointcloud_tools.pointcloud_diff:main',
+        'ego_compensator = scripts.ego_compensator:main',
+        'dynamic_cluster_node = scripts.dynamic_cluster_node:main',
+        'tracker = scripts.tracker:main',
+        'intent_estimator = scripts.intent_estimator:main',
+        'safety_interface = scripts.safety_interface:main',
+        'static_obstacle_detector = scripts.static_obstacle_detector:main',
+        'rgb_object_locator = scripts.rgb_object_locator:main',
+       
+        ],
+    },
+)
